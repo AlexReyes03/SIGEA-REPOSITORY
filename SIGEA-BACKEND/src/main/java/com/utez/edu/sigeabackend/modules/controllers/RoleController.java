a18cd685975b8390d21c59a8275cdc0401621b2d
dsanchezz <20213tn039@utez.edu.mx>
2025-05-28 22:23:06 -0600

    [ADD] Integro Modulo Role completo

--- SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/controllers/RoleController.java
+++ SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/controllers/RoleController.java
@@ -1,4 +1,43 @@
 package com.utez.edu.sigeabackend.modules.controllers;
 
+import com.utez.edu.sigeabackend.modules.entities.RoleEntity;
+import com.utez.edu.sigeabackend.modules.services.RoleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sigea/api/roles")
 public class RoleController {
+
+    private final RoleService service;
+
+    public RoleController(RoleService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody RoleEntity role) {
+        return service.create(role);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody RoleEntity role) {
+        return service.update(id, role);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return service.delete(id);
+    }
+
 }
--- SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/services/RoleService.java
+++ SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/services/RoleService.java
@@ -1,4 +1,66 @@
 package com.utez.edu.sigeabackend.modules.services;
 
+import com.utez.edu.sigeabackend.config.CustomResponseEntity;
+import com.utez.edu.sigeabackend.modules.entities.RoleEntity;
+import com.utez.edu.sigeabackend.modules.repositories.RoleRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RoleService {
+    private final RoleRepository repository;
+    private final CustomResponseEntity responseService;
+
+    public RoleService(RoleRepository repository, CustomResponseEntity responseService) {
+        this.repository = repository;
+        this.responseService = responseService;
+    }
+
+    public ResponseEntity<?> findAll() {
+        List<RoleEntity> roles = repository.findAll();
+        if (roles.isEmpty())
+            return responseService.get404Response();
+        return responseService.getOkResponse("Roles encontrados", roles);
+    }
+
+
+    public ResponseEntity<?> findById(long id) {
+        var optionalRole = repository.findById(id);
+        if (optionalRole.isPresent()) {
+            return responseService.getOkResponse("Rol encontrado", optionalRole.get());
+        } else {
+            return responseService.get404Response();
+        }
+    }
+
+    public ResponseEntity<?> create(RoleEntity role) {
+        if (repository.existsByRoleName(role.getRoleName()))
+            return responseService.get400Response();
+        repository.save(role);
+        return responseService.get201Response("Rol creado correctamente");
+    }
+
+    public ResponseEntity<?> update(long id, RoleEntity role) {
+        var optionalRole = repository.findById(id);
+        if (optionalRole.isPresent()) {
+            RoleEntity existing = optionalRole.get();
+            existing.setRoleName(role.getRoleName());
+            repository.save(existing);
+            return responseService.getOkResponse("Rol actualizado", existing);
+        } else {
+            return responseService.get404Response();
+        }
+    }
+
+    public ResponseEntity<?> delete(long id) {
+        var optionalRole = repository.findById(id);
+        if (optionalRole.isPresent()) {
+            repository.deleteById(id);
+            return responseService.getOkResponse("Rol eliminado", null);
+        } else {
+            return responseService.get404Response();
+        }
+    }
 }
