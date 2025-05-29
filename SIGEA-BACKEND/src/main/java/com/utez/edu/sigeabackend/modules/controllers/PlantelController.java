d186a5d0be644539944874d1f157a08f9266564f
dsanchezz <20213tn039@utez.edu.mx>
2025-05-28 22:14:30 -0600

    Â[ADD] Seccion Plantel completa

--- SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/controllers/PlantelController.java
+++ SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/controllers/PlantelController.java
@@ -1,4 +1,42 @@
 package com.utez.edu.sigeabackend.modules.controllers;
 
+import com.utez.edu.sigeabackend.modules.entities.PlantelEntity;
+import com.utez.edu.sigeabackend.modules.services.PlantelService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sigea/api/planteles")
 public class PlantelController {
+
+    private final PlantelService plantelService;
+
+    public PlantelController(PlantelService plantelService) {
+        this.plantelService = plantelService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return plantelService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable long id) {
+        return plantelService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody PlantelEntity plantel) {
+        return plantelService.create(plantel);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable long id, @RequestBody PlantelEntity plantel) {
+        return plantelService.update(id, plantel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        return plantelService.delete(id);
+    }
 }
--- SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/repositories/PlantelRepository.java
+++ SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/modules/repositories/PlantelRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlantelRepository extends JpaRepository<PlantelEntity, Long> {
+    boolean existsByName(String name);
 }
