e68f7b888f7e1b97049d4efd958480bbe4990f1d
dsanchezz <105498756+dsanchezz@users.noreply.github.com>
2025-07-25 13:32:00 -0600

    Cors update

--- SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/utils/security/SecurityConfig.java
+++ SIGEA-BACKEND/src/main/java/com/utez/edu/sigeabackend/utils/security/SecurityConfig.java
@@ -54,8 +54,6 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173",
-                "http://localhost:3000",
                 "https://corporativocetec.com"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
